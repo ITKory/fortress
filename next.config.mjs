134707f7a0ac252e88c28472df5b37e6499dbbd5
Alex Koryakin <Skoryakin3@gmail.com>
2025-11-19 01:49:32 +0300

    main: try fix image ith next image

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,17 @@ const nextConfig = {
   output: 'standalone',
 
   images: {
+    unoptimized: true,
     remotePatterns: [
-      { protocol: "https", hostname: "**" },
+      { protocol: "https", hostname: "downloader.disk.yandex.ru" },
+      { protocol: "https", hostname: "pst.yandex.ru" },
+      { protocol: "https", hostname: "avatars.mds.yandex.net" },
     ],
   },
+
+      experimental: {
+        largePageDataBytes: 5 * 1024 * 1024,
+      },
 };
 
 export default nextConfig;
