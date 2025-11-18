b62a34c50481783fa84991515840390253b31a3c
Alex Koryakin <Skoryakin3@gmail.com>
2025-11-18 17:47:00 +0300

    refactor code

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    outputFileTracingRoot: undefined,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+
   images: {
-    domains: ["avatars.mds.yandex.net", "downloader.disk.yandex.ru", "disk.yandex.ru"],
     remotePatterns: [
-      { protocol: "https", hostname: "**.yandex.net" },
-      { protocol: "https", hostname: "**.yandex.ru" },
-      { protocol: "https", hostname: "disk.yandex.ru" },
-      { protocol: "https", hostname: "downloader.disk.yandex.ru" },
-      { protocol: "https", hostname: "proxy.storage.yandex.net" },
-      { protocol: "https", hostname: "*cloud-api.yandex.net" },
+      { protocol: "https", hostname: "**" },
     ],
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
